e9de742746dea93db8dbe5759a9d256c7d57cd59
Nathan Rearick <nrearick@gmail.com>
2024-05-17 09:35:37 -0500

    complete dark mode functionality

---
+++ client/src/components/ThemeProvider.tsx
@@ -0,0 +1,19 @@
+import React, { ReactNode } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../redux/store'; // Adjust the path to where your RootState is defined
+
+interface ThemeProviderProps {
+  children: ReactNode;
+}
+
+export default function ThemeProvider({ children }: ThemeProviderProps) {
+  const { theme } = useSelector((state: RootState) => state.theme);
+
+  return (
+    <div className={theme}>
+      <div className="bg-white text-gray-700 dark:text-gray-200 dark:bg-[rgb(16,23,42)] min-h-screen">
+        {children}
+      </div>
+    </div>
+  );
+}
