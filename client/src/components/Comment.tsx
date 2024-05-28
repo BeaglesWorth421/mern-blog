84fbab64f38d8f104a987955aeacccd838413ccf
Nathan Rearick <nrearick@gmail.com>
2024-05-28 07:44:14 -0700

    add delete functionality to the comment section

--- client/src/components/Comment.tsx
+++ client/src/components/Comment.tsx
@@ -5,7 +5,7 @@ import { useSelector } from "react-redux";
 import { Button, Textarea } from "flowbite-react";
 
 
-export default function Comment({ comment, onLike, onEdit }) {
+export default function Comment({ comment, onLike, onEdit, onDelete }) {
     const [user, setUser] = useState({});
     const { currentUser } = useSelector((state) => state.user);
     const [isEditing, setIsEditing] = useState(false);
@@ -89,6 +89,7 @@ const handleSave = async () => {
                 </p>
                 {
                     currentUser && (currentUser._id === comment.userId || currentUser.isAdmin) && (
+                        <>
                         <button
                             type='button'
                             onClick={handleEdit}
@@ -96,6 +97,14 @@ const handleSave = async () => {
                         >
                             Edit
                         </button>
+                        <button
+                        type='button'
+                        onClick={() => onDelete(comment._id)}
+                        className="text-gray-400 hover:text-red-500"
+                    >
+                        Delete
+                    </button>
+                    </>
                     )
                 }
             </div>
