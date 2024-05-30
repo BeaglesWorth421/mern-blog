11847c5c8e531e257541386846bafa94b916362d
Nathan Rearick <nrearick@gmail.com>
2024-05-29 23:22:05 -0700

    complete about and projects pages

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -1,5 +1,16 @@
 export default function About() {
   return (
-    <div>About</div>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font-semibold text-center my-7">About Nathan's Blog</h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>Nathan's Blog is a blog that I created to share my thoughts and ideas with the world. I am a software engineer and I love to write about my experiences and things that I have learned. I hope you enjoy reading my blog.</p>
+            <p>Paragraph 2</p>
+            <p>Paragraph 3</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
