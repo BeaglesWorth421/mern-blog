fb9329a7c0f625d60f65e92ad38c913c26ba498e
Nathan Rearick <nrearick@gmail.com>
2024-05-30 14:47:05 -0700

    render fix 1

--- client/src/components/Comment.tsx
+++ client/src/components/Comment.tsx
@@ -3,114 +3,139 @@ import moment from "moment";
 import { FaThumbsUp } from "react-icons/fa";
 import { useSelector } from "react-redux";
 import { Button, Textarea } from "flowbite-react";
+import { RootState } from "../redux/store"; // Adjust the path according to your project structure
 
+interface CommentProps {
+  comment: {
+    _id: string;
+    content: string;
+    userId: string;
+    createdAt: string;
+    likes: string[];
+    numberOfLikes: number;
+  };
+  onLike: (id: string) => void;
+  onEdit: (comment: any, content: string) => void;
+  onDelete: (id: string) => void;
+}
 
-export default function Comment({ comment, onLike, onEdit, onDelete }) {
-    const [user, setUser] = useState({});
-    const { currentUser } = useSelector((state) => state.user);
-    const [isEditing, setIsEditing] = useState(false);
-    const [editedContent, setEditedContent] = useState(comment.content);
-    useEffect(() => {
-        const getUser = async () => {
-         try {
-            const res = await fetch(`/api/user/${comment.userId}`);
-            const data = await res.json();
-            if (res.ok) {
-                setUser(data);
-            }
-         } catch (error) {
-            console.log(error.message);
-            
-         }   
+interface User {
+  _id: string;
+  username: string;
+  profilePicture: string;
+  isAdmin: boolean; // Ensure isAdmin is part of the User interface
+}
+
+export default function Comment({ comment, onLike, onEdit, onDelete }: CommentProps) {
+  const [user, setUser] = useState<User | null>(null);
+  const { currentUser } = useSelector((state: RootState) => state.user);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedContent, setEditedContent] = useState(comment.content);
+
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const res = await fetch(`/api/user/${comment.userId}`);
+        const data = await res.json();
+        if (res.ok) {
+          setUser(data);
         }
-        getUser();
-    }, [comment])
+      } catch (error) {
+        if (error instanceof Error) {
+          console.log(error.message);
+        }
+      }
+    };
+    getUser();
+  }, [comment.userId]);
 
-const handleEdit = () => {
+  const handleEdit = () => {
     setIsEditing(true);
     setEditedContent(comment.content);
-};
+  };
 
-const handleSave = async () => {
+  const handleSave = async () => {
     try {
-        const res = await fetch(`/api/comment/editComment/${comment._id}`, {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                content: editedContent
-            })
-        });
-        if (res.ok) {
-            setIsEditing(false);
-            onEdit(comment, editedContent);
-        }
+      const res = await fetch(`/api/comment/editComment/${comment._id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          content: editedContent
+        })
+      });
+      if (res.ok) {
+        setIsEditing(false);
+        onEdit(comment, editedContent);
+      }
     } catch (error) {
-      console.log(error.message);
-        
+      if (error instanceof Error) {
+        console.log(error.message);
+      }
     }
-}
+  };
+
   return (
     <div className="flex p-4 border-b dark:border-gray-600 text-sm">
-        <div className="flex-shrink-0 mr-3">
-            <img className="w-10 h-10 rounded-full bg--gray-200" src={user.profilePicture} alt={user.username} />
+      <div className="flex-shrink-0 mr-3">
+        <img className="w-10 h-10 rounded-full bg-gray-200" src={user?.profilePicture || "/default-profile.png"} alt={user?.username || "anonymous user"} />
+      </div>
+      <div className="flex-1">
+        <div className="flex items-center mb-1">
+          <span className="font-bold mr-1 text-xs truncate">{user ? `@${user.username}` : "anonymous user"}</span>
+          <span className="text-gray-500 text-xs">{moment(comment.createdAt).fromNow()}</span>
         </div>
-        <div className="flex-1">
-            <div className="flex items-center mb-1">
-                <span className="font-bold mr-1 text-xs truncate">{user ? `@${user.username}` : "anonymous user"}</span>
-                <span className="text-gray-500 text-xs">{moment(comment.createdAt).fromNow()}</span>
+        {isEditing ? (
+          <>
+            <Textarea
+              className="mb-2"
+              value={editedContent}
+              onChange={(e) => setEditedContent(e.target.value)}
+            />
+            <div className="flex justify-end gap-2 text-xs">
+              <Button type="button" size="sm" gradientDuoTone="purpleToBlue" onClick={handleSave}>
+                Save
+              </Button>
+              <Button type="button" size="sm" gradientDuoTone="purpleToBlue" outline onClick={() => setIsEditing(false)}>
+                Cancel
+              </Button>
             </div>
-            {isEditing ? (
-                <>
-                <Textarea 
-                    className="mb-2"
-                    value={editedContent}
-                    onChange={(e) => setEditedContent(e.target.value)}
-                />
-                <div className="flex justify-end gap-2 text-xs">
-                    <Button type="button" size="sm" gradientDuoTone="purpleToBlue" onClick={handleSave}>
-                        Save
-                    </Button>
-                    <Button type="button" size="sm" gradientDuoTone="purpleToBlue" outline onClick={() => setIsEditing(false)}>
-                        Cancel
-                    </Button>
-                </div>
-                </>
-            ) : (
-                <>
-                <p className="text-gray-500 pb-2">{comment.content}</p>
+          </>
+        ) : (
+          <>
+            <p className="text-gray-500 pb-2">{comment.content}</p>
             <div className="flex items-center pt-2 text-xs border-t dark:border-gray-700 max-w-fit gap-2">
-                <button type="button" onClick={() => onLike(comment._id)} className={`text-gray-400 hover:text-blue-500 ${currentUser && comment.likes.includes(currentUser._id) && '!text-blue-500'}`}>
-                    <FaThumbsUp className="text-sm" />
-                </button>
-                <p className="text-gray-400">
-                    {comment.numberOfLikes > 0 && comment.numberOfLikes + " " + (comment.numberOfLikes === 1 ? "like" : "likes")}
-                </p>
-                {
-                    currentUser && (currentUser._id === comment.userId || currentUser.isAdmin) && (
-                        <>
-                        <button
-                            type='button'
-                            onClick={handleEdit}
-                            className="text-gray-400 hover:text-blue-500"
-                        >
-                            Edit
-                        </button>
-                        <button
-                        type='button'
-                        onClick={() => onDelete(comment._id)}
-                        className="text-gray-400 hover:text-red-500"
+              <button type="button" onClick={() => onLike(comment._id)} className={`text-gray-400 hover:text-blue-500 ${currentUser && comment.likes.includes(currentUser._id) && '!text-blue-500'}`}>
+                <FaThumbsUp className="text-sm" />
+              </button>
+              <p className="text-gray-400">
+                {comment.numberOfLikes > 0 && comment.numberOfLikes + " " + (comment.numberOfLikes === 1 ? "like" : "likes")}
+              </p>
+              {
+                currentUser && (currentUser._id === comment.userId || currentUser.isAdmin) && (
+                  <>
+                    <button
+                      type='button'
+                      onClick={handleEdit}
+                      className="text-gray-400 hover:text-blue-500"
                     >
-                        Delete
+                      Edit
                     </button>
-                    </>
-                    )
-                }
+                    <button
+                      type='button'
+                      onClick={() => onDelete(comment._id)}
+                      className="text-gray-400 hover:text-red-500"
+                    >
+                      Delete
+                    </button>
+                  </>
+                )
+              }
             </div>
-            </>
-            )}
-        </div>
+          </>
+        )}
+      </div>
     </div>
   )
 }
